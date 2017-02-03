104bb0275ca27f0ef90ff0de998d71def73d6ba2
Richard Dunn <your_name@domain.comrichard@wearekick.co.uk>
2017-02-03 16:59:32 +0000

    initial setup complete

--- app/server.js
+++ app/server.js
@@ -1,3 +1,15 @@
 'use strict'
 
 // start coding here
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/poo', function (req, res) {
+  res.send('Poo is funny')
+})
+
+app.listen(80)
